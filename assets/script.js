b73537d3efc4ed9be51395e72db57e6fb701d3b2
Alex Norton <atn362@gmail.com>
2021-03-20 13:51:18 -0500

    upload

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+var city = document.getElementById("inputCity");
+localStorage.setItem("city", '');
+
+var address = document.getElementById("inputAddress");
+localStorage.setItem("address", '');
+
+var address2 = document.getElementById("inputAddress2");
+localStorage.setItem("address2", '');
+
+
